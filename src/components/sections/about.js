986667d1260117af46f410a44779de00af8a9789
Jayakrishna20 <jayakrishnan2041@gmail.com>
2022-11-29 20:28:31 +0530

    added new list in about sections

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,8 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Javascript', 'Anaconda', 'React', '.NET', 'Node.js', 'Flutter'];
+  const langs = ['Python', 'Java', 'Dart', 'C#', 'C', 'C++'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +136,17 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Jayakrishnan and I enjoy creating things on Computer. I am currently
+              pursuing my B.Tech at College of Engineering, Cherthala. I am a 4th year student. My
+              interest in programming started from the fact that I play computer games. It was
+              around 2021 when I decided to unravel the mysteries and learn the technologies
+              required to build applications.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Fast-forward to today, and I’ve gone to learn building web and android applications
+              using various new and high in-demand frameworks. I have mastered the fundamentals of
+              many majors programming languages which have market demand.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
@@ -166,6 +154,12 @@ const About = () => {
 
           <ul className="skills-list">
             {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+          <div>
+            <p>Here are a few languages I’ve know:</p>
+          </div>
+          <ul className="skills-list">
+            {skills && langs.map((lang, i) => <li key={i}>{lang}</li>)}
           </ul>
         </StyledText>
 
